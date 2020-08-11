96c51cca13b7184b8456d4d2dbd5fb0fb691e9c6
atsuki1224 <akeve1224@icloud.com>
2020-08-11 11:37:13 +0900

    commit

--- twoSum.scala
+++ twoSum.scala
@@ -1,8 +1,35 @@
+//解答1
+/*
 object Solution {
     def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-        if(nums(0) + nums(1) == target){
+        // helper function for finding answer
+        // diff: Map for (value, idx)
+        @scala.annotation.tailrec
+        def acc(diff: Map[Int, Int], idx: Int): Array[Int] = {
+            if(idx >= nums.length) Array()
+            else {
+                if(diff contains nums(idx)) Array(diff(nums(idx)), idx)
+                else acc(diff + ((target - nums(idx)) -> idx), idx + 1)
+            }
         }
-      Array(nums.indexWhere(_==2),nums.lastIndexWhere(_==7))
+        
+        acc(Map(), 0)
     }
-    twoSum(Array(2, 7, 11, 15), 9)
+}
+*/
+
+//解答2
+object Solution {
+  def main(args: Array[String]) = {
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+      def twoSum(index: Int, previous: Map[Int, Int]): Array[Int] = {
+        previous.get(target - nums(index)) match {
+          case Some(previousIndex) => Array(previousIndex, index)
+          case None => twoSum(index + 1, previous ++ Map(nums(index) -> index))
+        }
+      }
+      twoSum(0, Map.empty)
+    }
+  }
+  println("hello")
 }
